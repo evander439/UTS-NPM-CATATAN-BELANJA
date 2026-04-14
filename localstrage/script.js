66d77bfaf22cae9c4926327cb18a3c80caf39f6f
a <evanderkenzi6@gmail.com>
2026-04-14 12:49:32 +0700

    commit ke 5

---
+++ localstrage/script.js
@@ -0,0 +1,10 @@
+function simpan() {
+    let nama = 
+        document.getElementById("nama-barang")
+    let jumlah = 
+        document.getElementById("jumlah barang")
+    let harga =
+        document.getElementById("harga barang")
+    let barang =
+        JSON.parse(localStorage.getItem("barang"))
+}
